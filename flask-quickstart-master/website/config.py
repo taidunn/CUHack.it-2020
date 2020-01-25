24f9db091d0e2a1bec5f5fa393948d75826c268f
Tai <macuser@Macs-MacBook-Pro-6.local>
2020-01-25 17:47:54 -0500

    Removing

--- flask-quickstart-master/website/config.py
+++
@@ -1,4 +0,0 @@
-
-class Config:
-	SECRET_KEY = "enter_a_better_secret_key"
-	SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
--- flask-quickstart-master/website/main/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Blueprint
-main = Blueprint('main', __name__)
-
-@main.route("/", methods=['GET'])
-def home():
-	return render_template("index.html")
-
-
