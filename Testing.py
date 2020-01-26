65803d70724072bbad9b0c91dbad74c4080dc788
Andrew <andrewjfulmer@gmail.com>
2020-01-26 03:07:46 -0500

    update

--- Testing.py
+++ Testing.py
@@ -19,8 +19,8 @@ class course:
         header = header.split('.')
         self.department = header[0].split('  ')[0]
         self.courseNumber = int(header[0].split('  ')[1].strip('H'))
-        self.name = header[2]
-        self.hours = header[3]
+        self.name = header[1]
+        self.hours = header[2]
         
         self.info = info
         
@@ -80,6 +80,113 @@ departments = []
 departments.append([None]*1000)
 courses = []
 
+def getPrereqs(name,number):
+    info = []
+    
+    index = -1
+    for i in range(0,len(departments)):
+        if(departments[i][0] == name):
+            index = i
+            break
+    c = departments[index][number]
+    
+    reqInfo = c.reqs_info
+    if(reqInfo == "None"):
+        return None
+        
+    reqInfo = "".join(reqInfo)
+    
+    
+    
+    
+    ands = []
+    ors = []
+    grade = ""
+    perm = ""
+    
+    lastDep = ""
+    lastPrif = ""
+    classTemp = []
+    
+    parts = reqInfo.replace(",", "")
+    parts = parts.strip('.')
+    parts = parts.split(' ')
+    
+    print("########")
+    print(reqInfo)
+    print(parts)
+    print("------")
+    
+    for word in parts:
+        if(word == ""):
+            continue
+        if(not isInt(word)):
+            if(word == 'and'):
+                if(lastPrif == 'or'):
+                    ors.append(classTemp)
+                    classTemp = []
+                    lastDep = ''
+                lastPrif = "and"
+            elif(word == 'or'):
+                if(lastPrif == 'and'):
+                    ors.append(classTemp)
+                    classTemp = []
+                    lastDep = ''
+                lastPrif = 'or'
+            else:
+                if(lastPrif == 'and'):
+                    ands.append(classTemp)
+                    classTemp = []
+                    if(word == 'or'):
+                        lastPrif = 'or'
+                    else:
+                        lastDep = word
+                        classTemp.append(lastDep)
+                elif(lastPrif == 'or'):
+                    ors.append(classTemp)
+                    classTemp = []
+                    if(word == 'and'):
+                        lastPrif = 'and'
+                    else:
+                        lastDep = word
+                        classTemp.append(lastDep)
+                elif(lastDep == ""):
+                    lastDep = word
+                    classTemp.append(lastDep)
+                elif(lastDep == word):
+                    continue
+                else:
+                    ands.append(classTemp)
+                    classTemp = []
+                    lastDep = word
+                    classTemp.append(lastDep)
+        else:
+            classTemp.append(word)
+        #print(lastDep)
+        
+        
+    if(lastPrif == 'and'):
+        ands.append(classTemp)
+        classTemp = []
+    elif(lastPrif == 'or'):
+        ors.append(classTemp)
+        classTemp = []
+    else:
+        ands.append(classTemp)
+        classTemp = []
+    
+    print("------")
+    print(ors)
+    print(ands)
+    print('\n')
+    
+def isInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+   
 def stripNonAscii(str):
     return ''.join([i if ord(i) < 128 else ' ' for i in str])
     
@@ -96,32 +203,47 @@ def read_csv(path):
         if (departments[0][0] == None):
             departments[0][c.courseNumber] = c
             departments[0][0] = c.department
-            courses.append[c.department,c.courseNumber]
+            courses.append([c.department,c.courseNumber,c.name])
         else:
             for i in range(0,len(departments)):
                 if(departments[i][0] == c.department):
                     departments[i][c.courseNumber] = c
-                    courses.append[c.department,c.courseNumber]
+                    courses.append([c.department,c.courseNumber,c.name])
                     break
                 elif(i == len(departments)-1):
-                    print("appended")
+                    #print("appended")
                     departments.append([None]*1000)
                     departments[i+1][0] = c.department
                     departments[i+1][c.courseNumber] = c
-                    courses.append[c.department,c.courseNumber]
-
+                    courses.append([c.department,c.courseNumber])
                     
 COM = "COM.csv"
 PHIL = "PHIL.csv"
 CLAR = "CLAR.csv"
+AAS = "AAS.csv"
+AERO = "AERO.csv"
+APPL = "APPL.csv"
+ARAB = "ARAB.csv"
 
 read_csv(COM)
 read_csv(PHIL)
 read_csv(CLAR)
+read_csv(AAS)
+#read_csv(AERO)
+#read_csv(APPL)
+#read_csv(ARAB)
 
-print(departments[0])
-print(departments[1])
-print(departments[2])
+#print(departments[0])
+#print(departments[1])
+#print(departments[2])
+#print(departments[3])
+    
+
+for i in range(0,150):
+   #print(courses[i])  
+   getPrereqs(courses[i][0],int(courses[i][1]))
+#print(departments[4])
+#print(departments[5])
 
 
        
