ad8e4a7f6e3bbd2502fcd2a0c00d857b1dba4219
Andrew <andrewjfulmer@gmail.com>
2020-01-25 21:17:35 -0500

    update

--- Testing.py
+++ Testing.py
@@ -4,11 +4,11 @@ import csv
 # a grade of C or better is required in all prerequisite courses.
 # a grade of C or better is required in COMP  116 or 401
 
-class req:
-    def __init__(self, department, reqs, rules):
-        self.department = department
-        self.reqs = reqs
-        self.rules = rules
+class reqs:
+    def __init__(ors, ands, froms):
+        self.ors = ors
+        self.ands = ands
+        self.froms = froms
     
     def toString():
         return ""
@@ -18,43 +18,44 @@ class course:
     def __init__(self, header, info):
         header = header.split('.')
         self.department = header[0].split('  ')[0]
-        self.courseNumber = header[0].split('  ')[1]
+        self.courseNumber = int(header[0].split('  ')[1].strip('H'))
         self.name = header[2]
         self.hours = header[3]
         
         self.info = info
         
         if("Requisites:" in self.info):
-            self.getReqs_info()
+            self.reqs_info = self.getReqs_info()
         else:
-            self.getReqs_info = []
+            self.reqs_info = "None"
             
         if("Gen Ed:" in self.info):
             self.genEd_info = self.getGenEd_info()
         else:
             self.genEd_info = []
-            
+        
+    
     def getReqs_info(self):
         r = []
         
         str = self.getSection("Requisites:")
         str = str[str.find(',')+1:]
         reqs = str.split(';')
-        print("-----------------------------------")
         for line in reqs:
-            if("a grade of" in line.lower()):
-                grade = line[line.find("of ")+3:line.find("of ")+4]
-                if(not "or better in" in line.lower()):
-                    r.append(line[line.find(" in")+3:] + " " + grade)
+            if("grade of" in line):
+                grade = line[line.find('f ')+2:line.find('f ')+3]
+                if('in both' in line.lower() or 'in all' in line.lower()):
+                    r[len(r)-1] = r[len(r)-1] + " " + grade
+                elif('is required in' in line.lower()):
+                    r.append(line[line.find('in')+2:] + " " + grade)
                 else:
                     r[len(r)-1] = r[len(r)-1] + " " + grade
             elif("permission of the instructor" in line.lower()):
-                r[len(r)-1] = r[len(r)-1] + " PERM"
+                r[len(r)-1] = r[len(r)-1] + " PERMS"
             else:
-                print(r)
                 r.append(line)
-        print("-----------------------------------")
         return r
+        
         
     def getGenEd_info(self):
         genEds = []
@@ -74,16 +75,25 @@ def stripNonAscii(str):
 
 f = open("Data.csv")
 
-courses = []
+departments = []
+departments.append([None]*1000)
 c = 0
 for row in csv.reader(f):
     if(c == 0):
         c += 1
         continue
     
-    courses.append(course(stripNonAscii(row[0]),stripNonAscii(row[1])))
-    
-    
+    c = course(stripNonAscii(row[0]),stripNonAscii(row[1]))
+    print(c.reqs_info)
+    if (departments[0][0] == None):
+        departments[0][c.courseNumber] = c
+        departments[0][0] == c.department
+    else:
+        for e in departments:
+            if(e[0] == c.department):
+                e[c.courseNumber] = c.courseNumber
+#print(departments)
+
     
     
     
