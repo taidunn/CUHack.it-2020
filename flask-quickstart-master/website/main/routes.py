4d028fd90fbd72543f99d1b1341394e97933c75b
unknown <andrewjfulmer@gmail.com>
2020-01-26 08:53:27 -0500

    Edits to flask and html

--- flask-quickstart-master/website/main/routes.py
+++ flask-quickstart-master/website/main/routes.py
@@ -1,3 +1,5 @@
+#from testing import ts
+
 from flask import render_template, Blueprint
 main = Blueprint('main', __name__)
 
@@ -10,3 +12,20 @@ def dog():
     return render_template("dog.html")
 
 
+#depart_course = ts.read_csv("courses/COM")
+#departments = depart_course[0]
+#courses = depart_course[1]
+
+##apply prereqs
+#for e in courses():
+#    definePrereqs(departments,e[0],e[1])
+
+#c = lookUpCourse(departments,courses[0][0],courses[0][1])
+#print(c.name)
+
+#input = "??"
+#courses = []
+
+#@main.route("/testing")
+#def testing():
+#    return render_template("testing.html",courses)
